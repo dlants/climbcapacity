a2b718eab9b0056173538ae86eccd389d5267384
Denis Lantsman <dlantsman@amplify.com>
2024-11-09 18:06:10 -0800

    put report card above explore

--- client/views/navigation.tsx
+++ client/views/navigation.tsx
@@ -4,7 +4,7 @@ export const Nav = ({ loggedIn }: { loggedIn: boolean }) => {
   return (
     <nav>
       <a href="/">Home</a> <a href="/snapshots">Snapshots</a>{" "}
-      <a href="/explore">Explore</a> <a href="/report-card">Report Card</a>{" "}
+      <a href="/report-card">Report Card</a> <a href="/explore">Explore</a>{" "}
       {loggedIn ? (
         <a href="/api/logout">Logout</a>
       ) : (
