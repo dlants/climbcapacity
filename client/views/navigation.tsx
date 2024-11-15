57322c74c1974ed17ee4e8129f6ae7dfed9e86e3
Denis Lantsman <dlantsman@amplify.com>
2024-11-14 20:45:17 -0800

    simpler navigation for logged out users

--- client/views/navigation.tsx
+++ client/views/navigation.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 export const Nav = ({ loggedIn }: { loggedIn: boolean }) => {
-  return (
-    <nav>
-      <a href="/">Home</a> <a href="/snapshots">Snapshots</a>{" "}
-      <a href="/report-card">Report Card</a> <a href="/explore">Explore</a>{" "}
-      {loggedIn ? (
+  if (loggedIn) {
+    return (
+      <nav>
+        <a href="/">Home</a> <a href="/snapshots">Snapshots</a>{" "}
+        <a href="/report-card">Report Card</a> <a href="/explore">Explore</a>{" "}
         <a href="/api/logout">Logout</a>
-      ) : (
+      </nav>
+    );
+  } else {
+    return (
+      <nav>
+        <a href="/">Home</a> <a href="/explore">Explore</a>{" "}
         <a href="/send-link">Login</a>
-      )}
-    </nav>
-  );
+      </nav>
+    );
+  }
 };
