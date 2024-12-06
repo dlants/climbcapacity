f94f179054f3c0b78dd418e467c7c9ad6a5f8418
Denis Lantsman <dlantsman@amplify.com>
2024-12-05 21:53:11 -0800

    add playwright

---
+++ client/playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html'],
+    ['github'],
+    ['list']
+  ],
+  use: {
+    baseURL: 'http://localhost:5173', // Default Vite dev server port
+    trace: 'on',  // Record traces for all tests
+    screenshot: 'on', // Capture screenshots for all tests
+    video: 'retain-on-failure', // Record video for failed tests
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'yarn dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
+});
